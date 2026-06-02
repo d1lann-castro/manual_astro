6b1fe925173aec1a6e745fb82b4e40639d519b13
Dilan Castro <castrodilan207@gmail.com>
2026-06-02 07:24:25 -0500

    Manual Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://d1lann-castro,github.io',
+  base: '/manual_astro',
   vite: {
     plugins: [tailwindcss()]
   }
